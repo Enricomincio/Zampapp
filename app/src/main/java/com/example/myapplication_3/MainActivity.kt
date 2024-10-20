c0d7cc15260191d09ed20dc03418291795ae7bfc
Enrico <enrico2002es@gmail.com>
2024-10-20 15:55:26 +0200

    First commit

---
+++ app/src/main/java/com/example/myapplication_3/MainActivity.kt
@@ -0,0 +1,52 @@
+package com.example.myapplication_3
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.example.myapplication_3.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        replaceFragment(Home())
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            when(it.itemId){
+
+                R.id.home -> replaceFragment(Home())
+                R.id.regalo -> replaceFragment(Donazioni())
+                R.id.cane -> replaceFragment(Adozioni())
+                R.id.sos -> replaceFragment(Sos())
+                R.id.profilo -> replaceFragment(Profilo())
+
+                else ->{
+
+
+                }
+            }
+                true
+
+        }
+
+
+    }
+
+    private fun replaceFragment(fragment : Fragment){
+
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout,fragment)
+        fragmentTransaction.commit()
+
+    }
+}
