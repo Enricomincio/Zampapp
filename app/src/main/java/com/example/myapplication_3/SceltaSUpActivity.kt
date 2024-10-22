95a7b2c18ac36bfb341e7870eafcb7325c900d65
imTraini <deitraini.dt@gmail.com>
2024-10-22 18:06:28 +0200

    aggiunta scelta funzionante con solo signUp personale

--- app/src/main/java/com/example/myapplication_3/SceltaSUpActivity.kt
+++ app/src/main/java/com/example/myapplication_3/SceltaSUpActivity.kt
@@ -4,10 +4,14 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
+import com.google.firebase.auth.FirebaseAuth
 
 class SceltaSUpActivity : AppCompatActivity() {
-    private lateinit var btnPersonale : Button
-    private lateinit var btnCanile : Button
+    private lateinit var btnPersonale : CardView
+    private lateinit var btnCanile : CardView
+
+    private lateinit var auth:FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,7 +20,14 @@ class SceltaSUpActivity : AppCompatActivity() {
         btnPersonale = findViewById(R.id.materialCardView)
         btnCanile = findViewById(R.id.materialCardView2)
 
+        auth = FirebaseAuth.getInstance()
+
+
         btnPersonale.setOnClickListener(){
+            val intent = Intent(this,SignUpActivity::class.java)
+            startActivity(intent)
+        }
+        btnCanile.setOnClickListener(){
             val intent = Intent(this,SignUpActivity::class.java)
             startActivity(intent)
         }
